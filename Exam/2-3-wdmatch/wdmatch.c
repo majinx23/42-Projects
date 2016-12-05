8e401da5c8d582747705260ba12058d6533fbb3e
Antonin GAVREL <angavrel@e1r9p16.42.fr>
2016-12-05 20:35:20 +0100

    wdmatch

--- Exam/2-3-wdmatch/wdmatch.c
+++ Exam/2-3-wdmatch/wdmatch.c
@@ -1,21 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   wdmatch.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/05 20:21:01 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/05 20:33:50 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void	wdmatch(char *str, char *str2)
+void	wdmatch(char *s1, char *s2)
 {
-	int i;
-	int j;
+	int len = 0;
+	int i = 0;
 
-	i = 0;
-	j = 0;
-	while (str2[j])
-	{
-		if (str2[j] == str[i])
-		{
-			write(1, &str[i], 1);
-			i++;
-		}
-		j++;
-	}
+	while (s1[len])
+		++len;
+	while (*s2 && i < len)
+		(*s2++ == s1[i]) ? ++i : 0;
+	if (i == len)
+		write(1, s1, len);
 }
 
 int		main(int ac, char **av)
