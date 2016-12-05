aea32c2366f6cb06817e5537d384e86f9bb12817
Antonin GAVREL <angavrel@e1r9p16.42.fr>
2016-12-05 16:27:14 +0100

    minor changes

--- Exam/5-2-ft_itoa_base/ft_itoa_base.c
+++ Exam/5-2-ft_itoa_base/ft_itoa_base.c
@@ -6,17 +6,12 @@
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/03 22:36:30 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/04 23:29:07 by angavrel         ###   ########.fr       */
+/*   Updated: 2016/12/05 16:25:37 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <stdio.h> //
-
-/*
-** Please email me if you find a shorter solution ;)
-** (not the one using ternary inside ternary and removing the variable "sign")
-*/
 
 char	*ft_itoa_base(int value, int base)
 {
@@ -25,21 +20,21 @@ char	*ft_itoa_base(int value, int base)
 	int		sign;
 	int		i;
 
-	n = (value < 0) ? -1 * (long)value : value;
+	n = (value < 0) ? -(long)value : value;
 	sign = (value < 0 && base == 10) ? -1 : 0;
 	i = (sign == -1) ? 2 : 1;
 	while ((n /= base) >= 1)
 		i++;
-	if (!(s = (char*)malloc(sizeof(char) * i + 1)))
+	if (!(s = (char*)malloc(sizeof(char) * (i + 1))))
 		return (NULL);
 	s[i] = '\0';
-	n = (value < 0) ? -1 * (long)value : value;
+	n = (value < 0) ? -(long)value : value;
 	while (i-- + sign)
 	{
 		s[i] = (n % base < 10) ? n % base + '0' : n % base + 'A' - 10;
 		n /= base;
 	}
-	s[i] = (sign == -1) ? '-' : s[i];
+	(i == 0) ? s[i] = '-' : 0;
 	return (s);
 }
 
