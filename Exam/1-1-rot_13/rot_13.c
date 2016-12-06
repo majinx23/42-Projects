fe8f87884ac1e540fd3e4b62b6c50576644c5dcf
Antonin GAVREL <angavrel@e2r10p16.42.fr>
2016-12-06 15:29:52 +0100

    shorter

--- Exam/1-1-rot_13/rot_13.c
+++ Exam/1-1-rot_13/rot_13.c
@@ -1,40 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rot_13.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/06 15:24:57 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/06 15:28:19 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
-{	
+{
 	write(1, &c, 1);
 }
 
 
-int		rot(char c)
+int		rot_13(char c)
 {
-	if (c >= 'A' && c <= 'M')
+	if ((c >= 'A' && c <= 'M') || (c >= 'a' && c <= 'm'))
 		c += 13;
-	else if (c >= 'N' && c <= 'Z')
-		c = c - 26 + 13;
-	else if (c >= 'a' && c <= 'm')
-		c += 13;
-	else if(c >= 'n' && c <= 'z')
-		c = c - 26 + 13;
+	else if ((c >= 'N' && c <= 'Z') || (c >= 'n' && c <= 'z'))
+		c -= 13;
 	return (c);
 }
 
 
 
-int		main(int ac, char **av)
+int	main(int ac, char **av)
 {
-	int i;
-	int repeat;
-
-	i = 0;
 	if (ac == 2)
-	{
-		while (av[1][i])
-		{
-			ft_putchar(rot(av[1][i]));
-			i++;
-		}
-	}
+		while (*av[1])
+			ft_putchar(rot_13(*av[1]++));
 	ft_putchar('\n');
 	return (0);
 }
