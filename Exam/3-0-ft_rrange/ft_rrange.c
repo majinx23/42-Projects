4d08095ef10d9199d9c119287f43082ee5ba701f
Antonin GAVREL <angavrel@e2r10p16.42.fr>
2016-12-06 16:03:18 +0100

    wrong

---
+++ Exam/3-0-ft_rrange/ft_rrange.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rrange.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/06 15:46:02 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/06 15:58:21 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int     *ft_rrange(int start, int end)
+{
+	int	*r;
+	int	len;
+
+	len = end - start + 1;
+	if (!(r = (int*)malloc(sizeof(int) * len)))
+		return (NULL);
+	 while (len--)
+		 r[len] = (end > start) ? end-- : end++;
+	return (r);
+}
