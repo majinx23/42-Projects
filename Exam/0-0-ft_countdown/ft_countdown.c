3fc1882823648811d20c68d1ec89732d05160bee
Antonin GAVREL <angavrel@e2r6p17.42.fr>
2016-12-07 17:49:19 +0100

    unused parameter

--- Exam/0-0-ft_countdown/ft_countdown.c
+++ Exam/0-0-ft_countdown/ft_countdown.c
@@ -6,7 +6,7 @@
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/07 17:44:56 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/07 17:45:19 by angavrel         ###   ########.fr       */
+/*   Updated: 2016/12/07 17:49:07 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int		main(int ac, char **av)
+int		main(void)
 {
 	int i;
 
--- Exam/0-1-aff_last_param/aff_last_param.c
+++ Exam/0-1-aff_last_param/aff_last_param.c
@@ -1,19 +1,26 @@
-#include <unistd.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   aff_last_param.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/07 17:46:21 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/07 17:47:34 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+#include <unistd.h>
 
 void	ft_putstr(char *str)
 {
 	while (*str)
-		ft_putchar(*str++);
+		write(1, str++, 1);
 }
 
 int		main(int ac, char **av)
 {
 	if (ac > 1)
 		ft_putstr(av[ac - 1]);
-	ft_putchar('\n');
+	write(1, "\n", 1);
 }
