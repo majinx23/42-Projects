202b08aa0b61228ad6d79e3e35d95c734cd10b29
Antonin GAVREL <angavrel@e3r11p19.42.fr>
2016-12-21 12:22:09 +0100

    epur

--- Exam/3-0-epur_str/epur_str.c
+++ Exam/3-0-epur_str/epur_str.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   epur_str.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/21 12:05:27 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/21 12:14:46 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 int		ft_strlen(char *s)
@@ -17,17 +29,17 @@ int		ft_isblank(char c)
 	return (0);
 }
 
-void		epurstr(char *s)
+void	epurstr(char *s)
 {
 	int len = ft_strlen(s);
 
 	while (len && ft_isblank(s[len - 1]))
 		--len;
-	while (len && *s++ && ft_isblank(*s))
+	while (len && ft_isblank(*s) && *s++)
 		--len;
 	while (len--)
 	{
-		if (!ft_isblank(*s) || (len > 1 && !ft_isblank(*(s + 1)) && (!ft_isblank(*(s + 1)))))
+		if (!ft_isblank(*s) || (*(s + 1) && !ft_isblank(*(s + 1))))
 			write(1, s, 1);
 		s++;
 	}
@@ -35,7 +47,7 @@ void		epurstr(char *s)
 
 int		main(int ac, char **av)
 {
-	if (ac == 2 && av[1][0])
+	if (ac == 2 && *av[1])
 		epurstr(av[1]);
 	write(1, "\n", 1);
 	return (0);
