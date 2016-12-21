c1b0e0ca8c2194620a3eaf356e37efd0ebca7382
Antonin GAVREL <angavrel@e3r11p19.42.fr>
2016-12-21 11:29:11 +0100

    maff alpha

--- Exam/0-0-maff_alpha/maff_alpha.c
+++ Exam/0-0-maff_alpha/maff_alpha.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   maff_alpha.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/12/21 11:26:55 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/21 11:27:58 by angavrel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putchar(char c)
@@ -5,20 +17,13 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-int		main(int ac, char **av)
+int		main(void)
 {
 	int i;
-	int a;
 
 	i = 96;
 	while (i++ < 122)
-	{
-		if (i % 2 == 0)
-			a = i - 32;
-		else
-			a = i;
-		ft_putchar(a);
-	}
-	ft_putchar('\n');	
+		(i % 2 == 0) ? ft_putchar(i - 32) : ft_putchar(i);
+	ft_putchar('\n');
 	return (0);
 }
