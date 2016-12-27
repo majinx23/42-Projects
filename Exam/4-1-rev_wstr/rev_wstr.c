a0ef2bd8b4d6d811a7294938dedcd1d166c79376
Antonin GAVREL <angavrel@e1r11p21.42.fr>
2016-12-27 05:19:41 +0100

    improved solution

--- Exam/4-1-rev_wstr/rev_wstr.c
+++ Exam/4-1-rev_wstr/rev_wstr.c
@@ -6,18 +6,11 @@
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/04 21:56:35 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/04 22:45:27 by angavrel         ###   ########.fr       */
+/*   Updated: 2016/12/27 05:19:32 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdlib.h>
-
-void	ft_putstr(char *s)
-{
-	while (*s)
-		write(1, s++, 1);
-}
 
 int		ft_isblank(char c)
 {
@@ -26,61 +19,28 @@ int		ft_isblank(char c)
 	return (0);
 }
 
-int		r_size(char *s)
-{
-	unsigned int n;
-	unsigned int i;
-
-	n = 0;
-	i = 0;
-	while (s[i])
-		if (!ft_isblank(s[i++]) && (!n || ft_isblank(s[i - 2])))
-			++n;
-	return (n);
-}
-
-char	**ft_strsplit(char *s)
-{
-	int		i = 0;
-	int		j = 0;
-	char	**r;
-	int		w_len = 0;
-	int		r_len = r_size(s);
-
-	if (!(r = (char **)malloc(sizeof(char *) * (r_len + 1))))
-		return (0);
-	while (*s && r_len--)
-	{
-		while (ft_isblank(*s))
-			++s;
-		while (*s && !ft_isblank(*s))
-		{
-			++s;
-			++w_len;
-		}
-		if (!(r[i] = (char *)malloc(sizeof(char) * (w_len + 1))))
-			return (0);
-		j = 0;
-		while (w_len)
-			r[i][j++] = *(s - w_len--);
-		r[i++][j] = '\0';
-	}
-	return (r);
-}
-
 void	rev_wstr(char *s)
 {
-	char	**r = ft_strsplit(s);
-	int		n = r_size(s);
+	int	wc = 0;
+	int	i = 0;
+	int	len;
+	int	a;
 
-	while (n--)
+	while (s[i])
+		if (!ft_isblank(s[i++]) && (!wc || ft_isblank(s[i - 2])))
+			++wc;
+	while (s[--i])
 	{
-		ft_putstr(r[n]);
-		if (n)
-			ft_putstr(" ");
+		if (!ft_isblank(s[i]) && wc--)
+		{
+			a = 0;
+			len = 1;
+			while (s[i - 1] && !ft_isblank(s[i - 1]) && ++len)
+				--i;
+			while (len-- && write(1, &s[i + a++], 1));
+			(wc) ? write(1, " ", 1) : 0;
+		}
 	}
-	free((char **)r);
-	r = NULL;
 }
 
 int		main(int ac, char **av)
