9d4c6c069de510bec89184b9392408c4f1432c12
Antonin GAVREL <angavrel@e1r11p21.42.fr>
2016-12-27 11:16:00 +0100

    sort list

--- Exam/4-3-sort_list/list.h
+++ Exam/4-3-sort_list/list.h
@@ -1,29 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sort_list.c                                        :+:      :+:    :+:   */
+/*   list.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: angavrel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/12/03 21:43:06 by angavrel          #+#    #+#             */
-/*   Updated: 2016/12/27 11:15:37 by angavrel         ###   ########.fr       */
+/*   Created: 2016/12/27 11:12:28 by angavrel          #+#    #+#             */
+/*   Updated: 2016/12/27 11:13:53 by angavrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "list.h"
-
-int		ascending(int a, int b)
+typedef	struct		s_list
 {
-	return (a <= b);
-}
-
-t_list	*sort_list(t_list* lst, int (*cmp)(int, int))
-{
-	t_list	*tmp;
-
-	tmp = lst;
-	while (lst->next)
-		if (((*cmp)(lst->data, lst->next->data)) == 0)
-			ascending(lst->data, lst->next->data);
-	return (tmp);
-}
+	int				data;
+	struct s_list	*next;
+}					t_list;
